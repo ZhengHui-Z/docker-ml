69fae1052b4ea964527bc5aed746b92b362f77c1
ZhengHui-Z <344237028@qq.com>
2023-06-17 02:21:22 +0800

    add py3.8

--- image/base-deps/build.sh
+++ image/base-deps/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ $1 -eq "3.10" ]; then
+if [ $1 = "3.10" ]; then
 
     image='zhenghuiz/cuda:11.8.0-cudnn8-devel-ubuntu20.04-python3.10'
-    docker build -t $image -build-arg PYTHON_VERSION=latest .
-elif [ $1 -eq "3.9" ]; then
+    docker build -t $image --build-arg PYTHON_VERSION=latest .
+elif [ $1 = "3.9" ]; then
     image='zhenghuiz/cuda:11.8.0-cudnn8-devel-ubuntu20.04-python3.9'
-    docker build -t $image -build-arg PYTHON_VERSION=py39_23.3.1-0 .
-elif [ $1 -eq "3.8" ]; then
+    docker build -t $image --build-arg PYTHON_VERSION=py39_23.3.1-0 .
+elif [ $1 = "3.8" ]; then
     image='zhenghuiz/cuda:11.8.0-cudnn8-devel-ubuntu20.04-python3.8'
-    docker build -t $image -build-arg py38_23.1.0-1 .
+    docker build -t $image --build-arg py38_23.1.0-1 .
+else
+    exit 1   
 fi
 
 
