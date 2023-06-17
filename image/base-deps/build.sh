3647e085799571018fadc5631ebe5536441d70ee
ZhengHui-Z <344237028@qq.com>
2023-06-17 22:17:50 +0800

    up

--- image/base-deps/build.sh
+++ image/base-deps/build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 if [ $1 = "3.10" ]; then
-
+    echo "build 11.8.0-cudnn8-devel-ubuntu20.04-python3.10"
     image='zhenghuiz/cuda:11.8.0-cudnn8-devel-ubuntu20.04-python3.10'
-    docker build -t $image --build-arg PYTHON_VERSION=latest .
+    docker build --build-arg PYTHON_VERSION=latest -t $image .
 elif [ $1 = "3.9" ]; then
+    echo "build 11.8.0-cudnn8-devel-ubuntu20.04-python3.9"
     image='zhenghuiz/cuda:11.8.0-cudnn8-devel-ubuntu20.04-python3.9'
-    docker build -t $image --build-arg PYTHON_VERSION=py39_23.3.1-0 .
+    docker build --build-arg PYTHON_VERSION=py39_23.3.1-0 -t $image .
 elif [ $1 = "3.8" ]; then
+    echo "build 11.8.0-cudnn8-devel-ubuntu20.04-python3.8"
     image='zhenghuiz/cuda:11.8.0-cudnn8-devel-ubuntu20.04-python3.8'
-    docker build -t $image --build-arg py38_23.1.0-1 .
+    docker build --build-arg PYTHON_VERSION=py38_23.1.0-1 -t $image .
 else
     exit 1   
 fi
 
 
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     echo "build success."
 else
     echo "build failure."
@@ -25,7 +27,7 @@ fi
 
 docker push $image
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     echo "push success."
 else
     echo "push failure."
